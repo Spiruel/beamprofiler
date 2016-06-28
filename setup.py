a585c2f20ec055cd699ffdb38c8dd39c6f684949
spiruel <samuel.bancroft@scarrowhill.co.uk>
2016-06-28 15:41:50 +0100

    Updated gitignore, added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sample',
+    version='0.0.1',
+    description='Sample package for Python-Guide.org',
+    long_description=readme,
+    author='Samuel Bancroft',
+    author_email='S.Bancroft@bham.ac.uk',
+    url='https://github.com/spiruel/beamprofiler',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
