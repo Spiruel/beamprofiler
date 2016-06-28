d302ddd3a1601ef01b18164f3d8e9554ec197f2c
spiruel <samuel.bancroft@scarrowhill.co.uk>
2016-06-28 15:01:35 +0100

    Initial Commit

---
+++ get_profile.py
@@ -0,0 +1,7 @@
+from lib.device import Camera
+from lib.processor import Controller
+from lib.interface import plotXY, imshow, waitKey,destroyWindow, moveWindow
+import numpy as np      
+import datetime
+
+Controller().mainloop()
