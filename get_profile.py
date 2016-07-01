5e4de5684d849f71771033dc659f7d0676dc4be1
spiruel <samuel.bancroft@scarrowhill.co.uk>
2016-07-01 17:33:55 +0100

    Added navbar, cross profile, plot switching, gaussian fit, screenshot grabber, about window, peak cross

--- get_profile.py
+++ get_profile.py
@@ -27,27 +27,47 @@ class Controller(tk.Frame):
         self.camera_index = 0
         self.colourmap = None
         self.centroid = None
+        self.fig_type = 'cross profile'
 
         frame = tk.Frame.__init__(self, parent,relief=tk.GROOVE,width=100,height=100,bd=1)
         self.parent = parent
         self.var = tk.IntVar()
 
         self.parent.title('Laser Beam Profiler')
+        
+        menubar = tk.Menu(self.parent)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Export Data")
+        filemenu.add_separator()
+        filemenu.add_command(label="Quit", command=self.close_window)
+        menubar.add_cascade(label="File", menu=filemenu)
+        
+        controlmenu = tk.Menu(menubar, tearoff=0)
+        controlmenu.add_command(label="Take Screenshot", command=self.save_screenshot)
+        controlmenu.add_separator()
+        controlmenu.add_command(label="Clear Windows")
+        menubar.add_cascade(label="Control", menu=controlmenu)
+        
+        helpmenu = tk.Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="About", command=self.donothing)
+        menubar.add_cascade(label="Help", menu=helpmenu)
+
+        self.parent.config(menu=menubar)
 
         labelframe = tk.Frame(self)
         labelframe.pack(side=tk.LEFT) #.grid(row=0, column=0) 
         
-        # self.scale1 = tk.Scale(labelframe, label='exposure',
-            # from_=1, to=10,
-            # length=300, tickinterval=30,
-            # showvalue='yes', 
-            # orient='horizontal',
-            # command = self.change_exp)
-        # self.scale1.pack()
+        self.scale1 = tk.Scale(labelframe, label='exposure',
+            from_=-1000000000, to=-10000,
+            length=300, tickinterval=10000,
+            showvalue='yes', 
+            orient='horizontal',
+            command = self.change_exp)
+        self.scale1.pack()
         
         self.scale2 = tk.Scale(labelframe, label='gain',
-            from_=1, to=10,
-            length=300, tickinterval=30,
+            from_=-10000, to=10000,
+            length=300, tickinterval=1,
             showvalue='yes', 
             orient='horizontal',
             command = self.change_gain)
@@ -71,8 +91,10 @@ class Controller(tk.Frame):
         self.dropdown2 = tk.OptionMenu(labelframe, self.variable2, "normal", "jet", command = self.change_colourmap)
         self.dropdown2.pack()
 
-        self.exit = tk.Button(labelframe, text = "Exit", command = self.close_window, compound = tk.BOTTOM)
-        self.exit.pack()
+        self.variable3 = tk.StringVar(labelframe)
+        self.variable3.set("cross profile")
+        self.dropdown3 = tk.OptionMenu(labelframe, self.variable3, "cross profile", "2d gaussian fit", command = self.change_fig)
+        self.dropdown3.pack()
         
         self.make_fig()
         self.init_camera()
@@ -80,8 +102,15 @@ class Controller(tk.Frame):
 
     def make_fig(self):
         '''Creates a matplotlib figure to be placed in the GUI.'''
-        # self.fig = Figure(figsize=(4,4), dpi=100) 
-        self.fig, self.ax = plt.subplots(2)
+
+        plt.clf()
+        plt.cla()
+        
+        if self.fig_type == 'cross profile':
+            self.fig, self.ax = plt.subplots(1,2, gridspec_kw = {'width_ratios':[16, 9]})
+        elif self.fig_type == '2d gaussian fit':
+            self.fig = Figure(figsize=(4,4), dpi=100)
+
         # self.ax.set_ylim(0,255)
         canvas = FigureCanvasTkAgg(self.fig, self) 
         canvas.show() 
@@ -93,31 +122,42 @@ class Controller(tk.Frame):
         
     def refresh_plot(self):
         '''Updates the matplotlib figure with new data.'''
-        # test = analysis.fix_saturation(self.img)
-        
+       
         grayscale = np.array(Image.fromarray(self.img).convert('L'))
-        if self.centroid != None:
-            self.ax[0].plot(grayscale[self.centroid[1]])
-            self.ax[1].plot(grayscale[self.centroid[1]])
         
-        # params = analysis.fit_gaussian(grayscale, with_bounds=False)
-        # analysis.plot_gaussian(grayscale, params)
+        if self.fig_type == 'cross profile':
+            if self.centroid != None:
+                self.ax[0].plot(range(self.width), grayscale[self.centroid[1],:],'k-')
+                self.ax[1].plot(grayscale[:,self.centroid[0]], range(self.height),'k-')              
+                self.ax[0].set_ylim(0,255)
+                self.ax[1].set_xlim(0,255)
+                
+        elif self.fig_type == '2d gaussian fit':
+            if self.centroid != None:
+                size = 50
+                x, y = self.centroid
+                img = grayscale[y-size/2:y+size/2, x-size/2:x+size/2]
+                params = analysis.fit_gaussian(img, with_bounds=False)
+                analysis.plot_gaussian(plt.gca(), img, params)
         
-        # self.ax.set_ylim(0,255)
+        # self.ax[0].hold(True)
+        # self.ax[1].hold(True)
+        
         self.fig.canvas.draw() 
-        self.ax[0].clear()
-        self.ax[1].clear()
+        
+        for axis in self.fig.get_axes():
+            axis.clear()
     
     def change_exp(self, option):
         '''Changes the exposure time of the camera.'''
-        exp = float(self.scale1.get())/10000000000000000000000000000000
+        exp = float(option)
         print 'changing exp to', exp
         self.cap.set(15, exp)
         
     def change_gain(self, option):
         '''Changes the gain of the camera.'''
-        gain = self.scale2.get()
-        print 'changing gain to', gain*1000
+        gain = float(option)
+        print 'changing gain to', gain
         self.cap.set(14, gain)
 
     def init_camera(self):
@@ -140,11 +180,18 @@ class Controller(tk.Frame):
     
     def change_colourmap(self, option):
         '''Changes the colourmap used in the camera feed.'''
-        print 'changed colourmap', option
-        if option == 'jet':
-            self.colourmap = cv2.COLORMAP_JET
-        else:
-            self.colourmap = None
+        if self.colourmap != option:
+            print 'changed colourmap', option
+            if option == 'jet':
+                self.colourmap = cv2.COLORMAP_JET
+            else:
+                self.colourmap = None
+            
+    def change_fig(self, option):
+        '''Changes the fig used.'''
+        if self.fig_type != option:
+            print 'changed fig', option
+            self.fig_type = option
     
     def show_frame(self):
         '''Shows camera view with relevant labels and annotations included.'''
@@ -155,18 +202,22 @@ class Controller(tk.Frame):
         else:
             cv2image = cv2.applyColorMap(frame, self.colourmap)
         
-        cv2.putText(cv2image,"Laser Beam profiler", (35,40), cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255))
+        cv2.putText(cv2image,"Laser Beam profiler", (10,40), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
         dim = np.shape(cv2image)
         
         # convert to greyscale
         tracking = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         
+        cv2.putText(cv2image,'Peak Value: ' + str(np.max(tracking)) + str(analysis.get_max(tracking,3)), (10,325), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
+        
         centroid = analysis.find_centroid(tracking)
         if centroid[0] < self.width or centroid[1] < self.height:
             if centroid != (None, None):
                 cv2.circle(cv2image,centroid,10,255,thickness=10)
-                cv2.putText(cv2image,'Centroid position: ' + str(centroid), (10,310), cv2.FONT_HERSHEY_SIMPLEX, .4, (255,255,255))
+                cv2.putText(cv2image,'Centroid position: ' + str(centroid), (10,310), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
                 self.centroid = centroid
+            else:
+                self.centroid = None
         else:
             self.centroid = None
 
@@ -207,6 +258,17 @@ class Controller(tk.Frame):
         '''Closes the GUI.'''
         self.parent.quit()
         self.parent.destroy()
+        
+    def donothing(self):
+       filewin = tk.Toplevel(self.parent)
+       input = tk.Text(filewin)
+       input.configure(state=tk.DISABLED,
+        borderwidth=0,
+        background=root.cget('background'))
+       input.insert(tk.END, "Laser Beam Profiler created by Samuel Bancroft \n Summer 2016 Internship Project \n Dr Jon Goldwin, Birmingham University")
+       
+    def save_screenshot(self):
+        cv2.imwrite('output.png', self.img) 
         
 c = Controller(root)
 c.pack()
--- lib/analysis.py
+++ lib/analysis.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
 
-import scipy.ndimage as snd
 import scipy.optimize as opt
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.patches import Circle
 
 def find_centroid(tracking):
     '''Takes greyscale cv2 image and finds one centroid position.'''
@@ -69,17 +72,6 @@ def func(xy, x0, y0, sigma, H):
     A = 1 / (2 * sigma**2)
     I = H * np.exp(-A * ( (x - x0)**2 + (y - y0)**2))
     return I
-
-# Generate 2D gaussian
-def generate(x0, y0, sigma, H):
-
-    x = np.arange(0, max(x0, y0) * 2 + sigma, 1)
-    y = np.arange(0, max(x0, y0) * 2 + sigma, 1)
-    xx, yy = np.meshgrid(x, y)
-
-    I = func((xx, yy), x0=x0, y0=y0, sigma=sigma, H=H)
-
-    return xx, yy, I
     
 def fit_gaussian(image, with_bounds):
 
@@ -88,9 +80,9 @@ def fit_gaussian(image, with_bounds):
     y = np.arange(0, image.shape[0], 1)
     xx, yy = np.meshgrid(x, y)
 
-    # Guess intial parameters
-    x0 = int(image.shape[0]) # Middle of the image
-    y0 = int(image.shape[1]) # Middle of the image
+    # Guess initial parameters
+    x0 = int(image.shape[0])/2 # Middle of the image
+    y0 = int(image.shape[1])/2 # Middle of the image
     sigma = max(*image.shape) * 0.1 # 10% of the image
     H = np.max(image) # Maximum value of the image
     initial_guess = [x0, y0, sigma, H]
@@ -113,15 +105,13 @@ def fit_gaussian(image, with_bounds):
     predictions = func((xx, yy), *pred_params)
     rms = np.sqrt(np.mean((image.ravel() - predictions.ravel())**2))
 
-    print("True params : ", true_parameters)
-    print("Predicted params : ", pred_params)
-    print("Residual : ", rms)
+    # print("True params : ", true_parameters)
+    # print("Predicted params : ", pred_params)
+    # print("Residual : ", rms)
 
     return pred_params
 
-def plot_gaussian(image, params):
-
-    fig, ax = plt.subplots()
+def plot_gaussian(ax, image, params):
     ax.imshow(image, cmap=plt.cm.BrBG, interpolation='nearest', origin='lower')
 
     ax.scatter(params[0], params[1], s=100, c="red", marker="x")
@@ -129,3 +119,31 @@ def plot_gaussian(image, params):
     circle = Circle((params[0], params[1]), params[2], facecolor='none',
             edgecolor="red", linewidth=1, alpha=0.8)
     ax.add_patch(circle)
+    
+def get_max(image,sigma,alpha=3,size=10):
+    # preallocate a lot of peak storage
+    k_arr = np.zeros((10000,2))
+    image_temp = image.copy()
+    peak_ct=0
+    while True:
+        k = np.argmax(image_temp)
+        j,i = np.unravel_index(k, image_temp.shape)
+        if(image_temp[j,i] >= alpha*sigma):
+            k_arr[peak_ct]=[j,i]
+            # this is the part that masks already-found peaks.
+            x = np.arange(i-size, i+size)
+            y = np.arange(j-size, j+size)
+            xv,yv = np.meshgrid(x,y)
+            # the clip here handles edge cases where the peak is near the 
+            #    image edge
+            image_temp[yv.clip(0,image_temp.shape[0]-1),
+                               xv.clip(0,image_temp.shape[1]-1) ] = 0
+            peak_ct+=1
+        else:
+            break
+    # trim the output for only what we've actually found
+    if len(k_arr[:peak_ct]) > 0:
+        firstmaxvalue = ' at position (' + str(k_arr[:peak_ct][:,1][0]) + ', ' + str(k_arr[:peak_ct][:,0][0]) + ')'
+    else:
+        firstmaxvalue = ''
+    return firstmaxvalue #returns one value at the moment, should be good enough for now
